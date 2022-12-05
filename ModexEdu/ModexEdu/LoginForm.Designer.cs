a2583f1e15208bced91537b4fdde37e394ad435c
Lynne Nkingi Nshimiye <ln304583@student.polsl.pl>
2022-12-05 22:37:28 +0100

    Displayed Register form.

--- ModexEdu/ModexEdu/LoginForm.Designer.cs
+++ ModexEdu/ModexEdu/LoginForm.Designer.cs
@@ -90,6 +90,7 @@ namespace ModexEdu
             this.button1.TabIndex = 4;
             this.button1.Text = "Register";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
